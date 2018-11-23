cc3e420cab03367ac93700808fc8b5f46a6ad979
adadgio <romain.bruckert.esc@gmail.com>
2018-11-23 11:22:12 +0100

    no message

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,48 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { ImcCalculator } from './components/calculator/imc-calculator';
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+describe('ImcTestSuite', () => {
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('testMdrd01', () => {
+        // start test
+        let num = 1
+        expect(num).toEqual(1)
+        
+        // end test
+    })
 
-  it(`should have as title 'OutilDeCalcul'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('OutilDeCalcul');
-  });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to OutilDeCalcul!');
-  });
-});
+})
+
+
+
+// describe('AppComponent', () => {
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       imports: [
+//         RouterTestingModule
+//       ],
+//       declarations: [
+//         AppComponent
+//       ],
+//     }).compileComponents();
+//   }));
+
+  // it('should create the app', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app).toBeTruthy();
+  // });
+  //
+  // it(`should have as title 'OutilDeCalcul'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('OutilDeCalcul');
+  // });
+  //
+  // it('should render title in a h1 tag', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to OutilDeCalcul!');
+  // });
+// });
---
+++ src/app/components/calculator/creatinine-calculator.ts
@@ -0,0 +1,11 @@
+import { round2 } from './round2';
+
+export class CreatinineCalculator
+{
+    private MDRD = 'MDRD';
+
+    compute(type, input: any)
+    {
+        
+    }
+}
